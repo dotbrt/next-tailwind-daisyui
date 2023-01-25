6b2927b2c159befadd986941a8f96755caf6bbbd
dotBrt <thisbart@protonmail.com>
2023-01-25 20:47:45 +0100

    init

--- app/layout.jsx
+++ app/layout.jsx
@@ -7,7 +7,9 @@ export default function RootLayout({ children }) {
         <head /> will contain the components returned by the nearest parent
         head.jsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+        <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&display=swap" rel="stylesheet" />
+      </head>
       <body>{children}</body>
     </html>
   )
